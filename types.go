a4cc200b8ce2a44bc5cee1ea54095b6155b72b41
lichong <lichong@gridsum.com>
2022-03-05 21:18:01 +0800

    add some field to the board struct

--- types.go
+++ types.go
@@ -23,7 +23,206 @@ type Board struct {
 	FolderUrl    string   `json:"folderUrl"`
 	Url          string   `json:"url,omitempty"` //TODO: ??
 	Rows         []*Row   `json:"rows"`
+	Annotations Annotations `json:"annotations"`
+	Description string `json:"description"`
+	FiscalYearStartMonth int `json:"fiscalYearStartMonth"`
+	GnetID int `json:"gnetId"`
+	GraphTooltip int `json:"graphTooltip"`
+	Iteration int64 `json:"iteration"`
+	Links []interface{} `json:"links"`
+	LiveNow bool `json:"liveNow"`
+	Panels []*Panel `json:"panels"`
+	Refresh string `json:"refresh"`
+	SchemaVersion int `json:"schemaVersion"`
+	Templating Templating `json:"templating"`
+	WeekStart string `json:"weekStart"`
 }
+type Annotations struct {
+	List []struct {
+		BuiltIn int `json:"builtIn"`
+		Datasource string `json:"datasource"`
+		Enable bool `json:"enable"`
+		Hide bool `json:"hide"`
+		IconColor string `json:"iconColor"`
+		Name string `json:"name"`
+		Type string `json:"type"`
+	} `json:"list"`
+}
+
+type Templating struct {
+	List []struct {
+		Current struct {
+			IsNone bool `json:"isNone"`
+			Selected bool `json:"selected"`
+			Text string `json:"text"`
+			Value string `json:"value"`
+		} `json:"current,omitempty"`
+		Hide int `json:"hide"`
+		IncludeAll bool `json:"includeAll"`
+		Label string `json:"label"`
+		Multi bool `json:"multi"`
+		Name string `json:"name"`
+		Options []interface{} `json:"options"`
+		Query string `json:"query"`
+		Refresh int `json:"refresh"`
+		Regex string `json:"regex"`
+		SkipURLSync bool `json:"skipUrlSync"`
+		Type string `json:"type"`
+		AllValue interface{} `json:"allValue,omitempty"`
+		Datasource string `json:"datasource,omitempty"`
+		Definition string `json:"definition,omitempty"`
+		Sort int `json:"sort,omitempty"`
+		TagValuesQuery string `json:"tagValuesQuery,omitempty"`
+		Tags []interface{} `json:"tags,omitempty"`
+		TagsQuery string `json:"tagsQuery,omitempty"`
+		UseTags bool `json:"useTags,omitempty"`
+	} `json:"list"`
+}
+type Panel struct {
+	CacheTimeout interface{} `json:"cacheTimeout,omitempty"`
+	ColorBackground bool `json:"colorBackground,omitempty"`
+	ColorValue bool `json:"colorValue,omitempty"`
+	Colors []string `json:"colors,omitempty"`
+	Datasource interface{} `json:"datasource"`
+	Editable bool `json:"editable"`
+	Error bool `json:"error"`
+	FieldConfig struct {
+		Defaults struct {
+			Custom struct {
+			} `json:"custom"`
+		} `json:"defaults"`
+		Overrides []interface{} `json:"overrides"`
+	} `json:"fieldConfig"`
+	Format string `json:"format,omitempty"`
+	Gauge struct {
+		MaxValue int `json:"maxValue"`
+		MinValue int `json:"minValue"`
+		Show bool `json:"show"`
+		ThresholdLabels bool `json:"thresholdLabels"`
+		ThresholdMarkers bool `json:"thresholdMarkers"`
+	} `json:"gauge,omitempty"`
+	GridPos struct {
+		H int `json:"h"`
+		W int `json:"w"`
+		X int `json:"x"`
+		Y int `json:"y"`
+	} `json:"gridPos"`
+	Height string `json:"height,omitempty"`
+	ID int `json:"id"`
+	Interval interface{} `json:"interval,omitempty"`
+	IsNew bool `json:"isNew"`
+	Links []interface{} `json:"links"`
+	MappingType int `json:"mappingType,omitempty"`
+	MappingTypes []struct {
+		Name string `json:"name"`
+		Value int `json:"value"`
+	} `json:"mappingTypes,omitempty"`
+	MaxDataPoints int `json:"maxDataPoints,omitempty"`
+	NullPointMode string `json:"nullPointMode"`
+	NullText interface{} `json:"nullText,omitempty"`
+	Postfix string `json:"postfix,omitempty"`
+	PostfixFontSize string `json:"postfixFontSize,omitempty"`
+	Prefix string `json:"prefix,omitempty"`
+	PrefixFontSize string `json:"prefixFontSize,omitempty"`
+	RangeMaps []struct {
+		From string `json:"from"`
+		Text string `json:"text"`
+		To string `json:"to"`
+	} `json:"rangeMaps,omitempty"`
+	Sparkline struct {
+		FillColor string `json:"fillColor"`
+		Full bool `json:"full"`
+		LineColor string `json:"lineColor"`
+		Show bool `json:"show"`
+	} `json:"sparkline,omitempty"`
+	TableColumn string `json:"tableColumn,omitempty"`
+	Targets []struct {
+		Expr string `json:"expr"`
+		IntervalFactor int `json:"intervalFactor"`
+		LegendFormat string `json:"legendFormat"`
+		Metric string `json:"metric"`
+		RefID string `json:"refId"`
+		Step int `json:"step"`
+	} `json:"targets"`
+	Thresholds interface{} `json:"thresholds"`
+	Title string `json:"title"`
+	Transparent bool `json:"transparent,omitempty"`
+	Type string `json:"type"`
+	ValueFontSize string `json:"valueFontSize,omitempty"`
+	ValueMaps []struct {
+		Op string `json:"op"`
+		Text string `json:"text"`
+		Value string `json:"value"`
+	} `json:"valueMaps,omitempty"`
+	ValueName string `json:"valueName,omitempty"`
+	AliasColors struct {
+	} `json:"aliasColors,omitempty"`
+	Bars bool `json:"bars,omitempty"`
+	DashLength int `json:"dashLength,omitempty"`
+	Dashes bool `json:"dashes,omitempty"`
+	Decimals int `json:"decimals,omitempty"`
+	Fill int `json:"fill,omitempty"`
+	FillGradient int `json:"fillGradient,omitempty"`
+	Grid struct {
+	} `json:"grid,omitempty"`
+	HiddenSeries bool `json:"hiddenSeries,omitempty"`
+	Legend struct {
+		AlignAsTable bool `json:"alignAsTable"`
+		Avg bool `json:"avg"`
+		Current bool `json:"current"`
+		Max bool `json:"max"`
+		Min bool `json:"min"`
+		RightSide bool `json:"rightSide"`
+		Show bool `json:"show"`
+		Total bool `json:"total"`
+		Values bool `json:"values"`
+	} `json:"legend,omitempty"`
+	Lines bool `json:"lines,omitempty"`
+	Linewidth int `json:"linewidth,omitempty"`
+	Options struct {
+		DataLinks []interface{} `json:"dataLinks"`
+	} `json:"options,omitempty"`
+	Percentage bool `json:"percentage,omitempty"`
+	Pointradius int `json:"pointradius,omitempty"`
+	Points bool `json:"points,omitempty"`
+	Renderer string `json:"renderer,omitempty"`
+	SeriesOverrides []interface{} `json:"seriesOverrides,omitempty"`
+	SpaceLength int `json:"spaceLength,omitempty"`
+	Stack bool `json:"stack,omitempty"`
+	SteppedLine bool `json:"steppedLine,omitempty"`
+	TimeFrom interface{} `json:"timeFrom,omitempty"`
+	TimeRegions []interface{} `json:"timeRegions,omitempty"`
+	TimeShift interface{} `json:"timeShift,omitempty"`
+	Tooltip struct {
+		MsResolution bool `json:"msResolution"`
+		Shared bool `json:"shared"`
+		Sort int `json:"sort"`
+		ValueType string `json:"value_type"`
+	} `json:"tooltip,omitempty"`
+	Xaxis struct {
+		Buckets interface{} `json:"buckets"`
+		Mode string `json:"mode"`
+		Name interface{} `json:"name"`
+		Show bool `json:"show"`
+		Values []interface{} `json:"values"`
+	} `json:"xaxis,omitempty"`
+	Yaxes []struct {
+		Format string `json:"format"`
+		Label interface{} `json:"label"`
+		LogBase int `json:"logBase"`
+		Max interface{} `json:"max"`
+		Min interface{} `json:"min"`
+		Show bool `json:"show"`
+	} `json:"yaxes,omitempty"`
+	Yaxis struct {
+		Align bool `json:"align"`
+		AlignLevel interface{} `json:"alignLevel"`
+	} `json:"yaxis,omitempty"`
+}
+
+
+
+
 
 type CreateDashboardRequest struct {
 	Board Board `json:"dashboard"`
